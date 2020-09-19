86e77c6dca13e103b836c72cccfca330114ce68c
Thales <thalestestoni@gmail.com>
2020-09-19 11:26:36 -0300

    Estilizando o Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,69 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/49066917?s=460&u=d9274c95db4defd8493c698325f4be3766f05473&v=4"
+            alt="Thales"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>
+              Easy peasy highly scalable ReactJS & React Native forms! rocket
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/49066917?s=460&u=d9274c95db4defd8493c698325f4be3766f05473&v=4"
+            alt="Thales"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>
+              Easy peasy highly scalable ReactJS & React Native forms! rocket
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/49066917?s=460&u=d9274c95db4defd8493c698325f4be3766f05473&v=4"
+            alt="Thales"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>
+              Easy peasy highly scalable ReactJS & React Native forms! rocket
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
